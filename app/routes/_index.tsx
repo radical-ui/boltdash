dbced2474130d90fd4c70912ff3cb06b2a17c9a8
Elijah Mooring <vehmloewff@Elijahs-MacBook-Air.local>
2024-08-06 14:55:23 -0700

    sql editor progress

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,8 @@
 export default function Index() {
   return (
-    <textarea placeholder="Type sql here..."></textarea>
+    <>
+      <textarea placeholder="Type sql here..." class="w-full h-full p-2 outline-none font-mono" value="select * from 'table';" rows="15"></textarea>
+      <div class="border-gray border-t-2 p-2">Chart here</div>
+    </>
   );
 }
